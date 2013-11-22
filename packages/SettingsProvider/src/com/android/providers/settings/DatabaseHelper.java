d60f2362d93de914d69a0c1c8446bf2cc0a3ad21
Dave Daynard <nardholio@gmail.com>
2013-11-22 18:31:54 -0500

    Turn off carrier provisioning by default to allow tethering
    Whether or not to use carrier provisioning is a setting hidden to the
    end user that is currently not set. Unfortunately, on some carriers that
    may result in it being set to yes, or effectively set to yes, without
    other accompanying changes that must occur, such as setting a tethering
    apn and possibly having a carrier provisioning app, resulting in hitting
    a paywall when a tethering plan exists, or, worse having nothing happen
    at all when you try to tether since the data doesn't have an apn set.
    
    Change-Id: I3b5b3cc86e3325302411f46e3fd5f7a7c1b9f57e
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2598,6 +2598,9 @@ class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.Global.TETHER_DUN_REQUIRED,
                     R.integer.def_tether_dun_required);
 
+            loadIntegerSetting(stmt, Settings.Global.TETHER_DUN_REQUIRED,
+                    R.integer.def_tether_dun_required);
+
             /*
              * IMPORTANT: Do not add any more upgrade steps here as the global,
              * secure, and system settings are no longer stored in a database
