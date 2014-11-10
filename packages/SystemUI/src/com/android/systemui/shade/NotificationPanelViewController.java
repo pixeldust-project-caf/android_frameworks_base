c89751a4e15a375c5d16a1b29350d5842ebcafcb
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    SystemUI: Add quick settings pull down with one finger
    Logic is currently set to use 1/3 of right hand side of the statusbar to
    trigger a quick settings quick pull down, quickly.
    
    Change-Id: I3f536496f724f07d2419a5b2db5fa8f8261609e7
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: fix right hand side gestures on lockscreen
    
    two finger quick pulldown was not accounting for being in the keyguard
    adn was eating touch events when it shouldn't be.
    
    Change-Id: I625d48a4002aaaf2d55d7efaed65d892ab7aa13c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    From: Zhao Wei Liew <zhaoweiliew@gmail.com>
    SystemUI: statusbar: Fix QS quick pulldown feature
    
    Bring in the fixes from CM 13.
    
    This commit squashes the following patches from CM 13.0:
    68c40f32470d66e0f8973d33a4124a2181754fb0 SystemUI: fix QS quick pull down
    477f58dc1413943926f2b4428f5873c0bb04dab8 SystemUI: hook up settings observer in notificationpanel
    d2bec52c76a3bb2c9a7fa3533e40f782fa592826 SystemUI: Fix quick-quick settings pulldown settings
    
    Change-Id: Ie122477261d96499c80640cf274d22fba6642a01
    
    From: Zhao Wei Liew <zhaoweiliew@gmail.com>
    (partial) SystemUI: Use Tuner API for CM settings
    Get rid of all the excess code by implementing TunerService.Tunable
    and observing any changes made to the settings through it.
    
    Remove UserContentObserver as the Tuner API handles user switches.
    
    Also remove some unused imports that were introduced in earlier
    CM commits, but were never removed since.
    
    [Pig]: Forward port to R
    [bgcngm]: Forward port to S
    
    [spezi77] Stripped-down variant: removed left QS quick pulldown and removed user toggle
    
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2530,7 +2530,13 @@ public final class NotificationPanelViewController implements Dumpable {
                         MotionEvent.BUTTON_SECONDARY) || event.isButtonPressed(
                         MotionEvent.BUTTON_TERTIARY));
 
-        return twoFingerDrag || stylusButtonClickDrag || mouseButtonClickDrag;
+        final float w = mView.getMeasuredWidth();
+        final float x = event.getX();
+        float region = w * 1.f / 4.f; // TODO overlay region fraction?
+        boolean showQsOverride = mView.isLayoutRtl() ? x < region : w - region < x;
+        showQsOverride &= mBarState == StatusBarState.SHADE;
+
+        return twoFingerDrag || showQsOverride || stylusButtonClickDrag || mouseButtonClickDrag;
     }
 
     private void handleQsDown(MotionEvent event) {
