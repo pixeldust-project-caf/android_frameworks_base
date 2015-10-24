989887fe08799a231a7b87e852302df7e5047766
beanstown106 <nbenis106@gmail.com>
2015-10-24 23:10:16 +0200

    Allow to disable lockscreen media artwork
    abc: move the setting check to the custom observer
    and adapt code to latest android release
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I08e1cb4666447787477f394004e5e66a0f8c739b
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4636,6 +4636,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_MEDIA_BLUR),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SHOW_LOCKSCREEN_MEDIA_ART),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4653,13 +4656,17 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
                 setLockScreenMediaBlurLevel();
+            }  else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.SHOW_LOCKSCREEN_MEDIA_ART))) {
+                setLockScreenMediaArt();
             }
         }
 
         public void update() {
             setLockscreenDoubleTapToSleep();
             setStatusBarWindowViewOptions();
-            setLockScreenMediaBlurLevel();            
+            setLockScreenMediaBlurLevel();
+            setLockScreenMediaArt();         
         }
     }
 
@@ -4678,6 +4685,12 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void setLockScreenMediaBlurLevel() {
         if (mMediaManager != null) {
             mMediaManager.setLockScreenMediaBlurLevel();
+        }
+    }
+
+    private void setLockScreenMediaArt() {
+        if (mMediaManager != null) {
+            mMediaManager.setLockScreenMediaArt();
         }
     }
 
