977336714f0b4603028922d301e0b8e8d8d271e1
Alex Cruz <mazdarider23@gmail.com>
2015-12-22 13:31:32 -0800

    QS: Navigation Bar tile
    Rewrote using SystemSetting helper
    
    Change-Id: I082876b26945facbbbc06d1175964267e4c05152
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
+import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
@@ -125,6 +126,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AlwaysOnDisplayTile(mHost);
             case "sync":
                 return new SyncTile(mHost);
+            case "navbar":
+                return new NavBarTile(mHost);
         }
 
         // Intent tiles.
