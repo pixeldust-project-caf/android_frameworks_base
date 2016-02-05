8882d97a9386c58288a13fdd35bcf5ba0f2a49ad
Roman Birg <roman@cyngn.com>
2016-02-04 16:28:00 -0800

    SystemUI: handle camera launch gesture from keyhandler
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Feb 4 16:28:00 2016 -0800
    
        framework: move device key handler logic, fix gesture camera launch
    
        In order to make device camera gestures behave the same way as launching
        the camera by double tapping the power button, route DeviceKeyHandler
        logic through the GestureLauncherService.
    
        A callback is used in order to avoid publishing the
        GestureLauncherService into the System service registry, because the
        KeyHandler code cannot directly access services local to the system
        process.
    
        OPO-427
    
        Change-Id: I2a8860b278f65ccf31bfee151513d8a23ab746c5
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Sat Feb 13 09:08:21 2016 -0800
    
        KeyHandler: don't break old interfaces
    
        Causing bootloops on devices, don't break old interfaces in the
        framework.
    
        OPO-427
    
        Change-Id: I49473b057bc4ed4710e9092c76e41c6a8134d2de
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue Feb 16 17:22:01 2016 -0800
    
        SystemUI: handle camera launch gesture from keyhadler
    
        Includes partial reverts:
    
            Revert "KeyHandler: don't break old interfaces"
            This reverts commit 31ecbdc5fcc1263e9028107858565d951798fd93.
    
            Revert "framework: move device key handler logic, fix gesture camera launch"
            This reverts commit db1b4e6e4880be1828342be9bc468d36565a2ef0.
    
        Ref: OPO-427
    
        Change-Id: Ic15543a8f63ad4521ab0ab536d00224b0bd70f8c
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Michael Bestas <mikeioannina@cyanogenmod.org>
    Date:   Sat Feb 20 00:56:47 2016 +0200
    
        SystemUI: Don't vibrate on touchscreen camera gesture
    
        * This is usually handled in device-specific keyhandler,
          where vibration is optional and has the same duration
          in all gestures
    
        Change-Id: I879671c9d6f785137d63b8b70de35f5d5020748d
    
    [spezi77] Updated for 12.1.0_r1
    - Google moved some code from StatusBar to StatusBarCommandQueueCallbacks
    - onCameraLaunchGestureDetected() has been moved to CommandQueueCallbacks
    
    Change-Id: Iba9211b7d342f618c4082c34d11663d89861607a
    (cherry picked from commit b48ec290d5c734950b2ccfd56247fac72ea76331)
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1466,6 +1466,7 @@ public class StatusBar extends SystemUI implements
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG);
+        filter.addAction(Intent.ACTION_SCREEN_CAMERA_GESTURE);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
     }
 
@@ -2746,6 +2747,19 @@ public class StatusBar extends SystemUI implements
                 }
                 finishBarAnimations();
                 resetUserExpandedStates();
+            }
+            else if (Intent.ACTION_SCREEN_CAMERA_GESTURE.equals(action)) {
+                boolean userSetupComplete = Settings.Secure.getInt(mContext.getContentResolver(),
+                        Settings.Secure.USER_SETUP_COMPLETE, 0) != 0;
+                if (!userSetupComplete) {
+                    if (DEBUG) Log.d(TAG, String.format(
+                            "userSetupComplete = %s, ignoring camera launch gesture.",
+                            userSetupComplete));
+                    return;
+                }
+
+                mCommandQueueCallbacks.onCameraLaunchGestureDetected(
+                        StatusBarManager.CAMERA_LAUNCH_SOURCE_SCREEN_GESTURE);
             }
             else if (DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG.equals(action)) {
                 mQSPanelController.showDeviceMonitoringDialog();
