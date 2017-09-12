a9af363060a5c9a7b4a67d19b2a62774b8926946
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots abc: extracted from DUI add a public call to PixeldustUtils aex: Add delay
    [spezi77] Updated for Android 13
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: If8d22174ccddc9d2d8c259f8dcc617f3c5583693

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6429,6 +6429,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public void sendCustomAction(Intent intent) {
+        mPolicy.sendCustomAction(intent);
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
