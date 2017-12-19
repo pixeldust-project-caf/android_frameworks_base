e81780d2169fb5b36d86fbdd3756c6573f43bc78
ezio84 <brabus84@gmail.com>
2017-12-19 14:37:35 +0100

    Screenshot: Append app name to filename
    Author: ezio84 <brabus84@gmail.com>
    Date:   Tue Dec 19 14:37:35 2017 +0100
    
        Screenshot: append app name to filename
    
        Kang Samsung's idea.
    
        Change-Id: Ibc6a52b5e2597e6014a2da6a4211febe17ec02c7
    
    Author: Wang Han <wanghan1995315@gmail.com>
    Date:   Fri May 11 12:47:40 2018 +0200
    
        SystemUI: Don't append app name to file on lockscreen
    
        When device is locked, current implementation will append the current
        app name to file when taking screenshot. This causes information leaks.
    
        To prevent this, we check for isKeyGuardLocked() and fall back not to
        append the app name when taking screenshot on lockscreen.
    
        Change-Id: I04498faf51986e1a3723a32befd97d29e1f3fe58
    
    Author: DroidFreak32 <rushabshah32@gmail.com>
    Date:   Thu Oct 4 11:33:07 2018 +0530
    
        Screenshot: Append app name after screenshot date.
    
        Change-Id: I2ee5d65006ff22dfa381aae50e5757b8e7596e91
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sat Jun 16 16:57:19 2018 +0200
    
        GlobalScreenshot: Fix screenshot not saved with some languages
    
        like Virgin Islands English when taking a screenshot of the Settings app
    
        Change-Id: Ic04f66f5813b9597c96835d15c74509c93530a5c
    
    Author: Han Wang <416810799@qq.com>
    Date:   Mon May 25 11:18:01 2020 +0200
    
        SystemUI: Do not convert application name to ISO-8859-15
    
         * ISO-8859-15 does not handle CJK chars, so all of them are
           'unknown' and empty eventually.
    
        Change-Id: I27c689f5d737190d381146e49c1989e998b4f623
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon Aug 24 17:52:39 2020 +0200
    
        SystemUI: Use TaskStackChangeListener API for contextual screenshot package name
    
        * This should hopefully fix mislabeled screenshots.
    
        Change-Id: Ifde106c0e306a6700081e4d724c1cf0c06dd126c
    
    Change-Id: Ic2a42536bf763a0c6f7fb9ac4bbbe84f73723f91
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/tests/src/com/android/systemui/screenshot/ImageExporterTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/screenshot/ImageExporterTest.java
@@ -89,7 +89,7 @@ public class ImageExporterTest extends SysuiTestCase {
     @Test
     public void testImageFilename() {
         assertEquals("image file name", "Screenshot_20201215-131500.png",
-                ImageExporter.createFilename(CAPTURE_TIME, CompressFormat.PNG));
+                ImageExporter.createFilename(CAPTURE_TIME, CompressFormat.PNG, null));
     }
 
     @Test
@@ -171,7 +171,7 @@ public class ImageExporterTest extends SysuiTestCase {
 
     @Test
     public void testMediaStoreMetadata() {
-        String name = ImageExporter.createFilename(CAPTURE_TIME, CompressFormat.PNG);
+        String name = ImageExporter.createFilename(CAPTURE_TIME, CompressFormat.PNG, null);
         ContentValues values = ImageExporter.createMetadata(CAPTURE_TIME, CompressFormat.PNG, name);
         assertEquals("Pictures/Screenshots",
                 values.getAsString(MediaStore.MediaColumns.RELATIVE_PATH));
