b64bd762fdd86081637b27f960993bf650008513
SpiritCroc <spiritcroc@gmail.com>
2018-01-13 12:16:46 +0100

    QS: Add Always On Display QS Tile
    Change-Id: I437bbccd7ed7c804ade76a4d422e27e0f9b2497e
    
    PixelDust Edits:
    - Updated for PIE
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -24,6 +24,7 @@ import com.android.systemui.plugins.qs.*;
 import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AlwaysOnDisplayTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.PDSettingsTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -119,6 +120,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ReadingModeTile(mHost);
             case "lte":
                 return new LteTile(mHost);
+            case "always_on_display":
+                return new AlwaysOnDisplayTile(mHost);
         }
 
         // Intent tiles.
