1ea2cb8016b811c2f244b1f4f83fd830eeb4e51f
varund7726 <date.varun123@gmail.com>
2018-09-16 16:44:16 +0530

    base: Option to Display Data Disabled Indicator Icon [1/2]
    Change-Id: Ib14eb047715d93667774dfa699ea1b97a9c2ab53
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5516,6 +5516,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
         /**
+         * Whether to display cross sign for a data disabled connection
+         * @hide
+         */
+        public static final String DATA_DISABLED_ICON = "data_disabled_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
