a9326da2d07475e9276593a75c17351ddbe3cfcb
spezi77 <spezi7713@gmx.net>
2018-10-29 21:45:03 +0100

    Battery: Show percentage in QuickSbHeader & add a switch. Thanks to ezio84 & BenzoRom for the idea
    -Adapted to crdroid's simplified battery styles code
    -Fix battery percentage not being updated on QuickSBHeader.
    -Ensure that the "forcePercentage on QS header pull down"
    option behaves as if we are on stock Android for all battery
    styles (icon, text or hidden).
    -Set to on by default to match with AOSP default setting
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    
    Show battery percent on qsb: Turn on by default like AOSP does.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4236,6 +4236,15 @@ public final class Settings {
         };
 
         /**
+         * Setting to show the battery percentage in the quickstatusbar.
+         * @hide
+         */
+        public static final String SHOW_BATTERY_PERCENT_ON_QSB = "show_battery_percent_on_qsb";
+
+        /** @hide */
+        private static final Validator SHOW_BATTERY_PERCENT_ON_QSB_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -4708,6 +4717,7 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
+            SHOW_BATTERY_PERCENT_ON_QSB,
             NOTIFICATION_VIBRATION_INTENSITY,
             HAPTIC_FEEDBACK_INTENSITY,
             DISPLAY_COLOR_MODE,
@@ -4824,6 +4834,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
+            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT_ON_QSB);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
 
             // Pocket mode handler.
@@ -4955,6 +4966,7 @@ public final class Settings {
             VALIDATORS.put(NETWORK_TRAFFIC_SHOW_UNITS, NETWORK_TRAFFIC_SHOW_UNITS_VALIDATOR);
             VALIDATORS.put(DISPLAY_CUTOUT_HIDDEN, DISPLAY_CUTOUT_HIDDEN_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_SCREEN_ON, NOTIFICATION_LIGHT_SCREEN_ON_VALIDATOR);
+            VALIDATORS.put(SHOW_BATTERY_PERCENT_ON_QSB, SHOW_BATTERY_PERCENT_ON_QSB_VALIDATOR);
         }
 
         /**
