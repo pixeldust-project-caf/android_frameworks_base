36a4073b70e74b5854e63879aa442a2de36a7e04
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    ZeNiXxX - Bring to Android 10
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I318b58e5f79e78fa1cdcefb2bb0a110b1ac4104a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5154,6 +5154,16 @@ public final class Settings {
          public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
