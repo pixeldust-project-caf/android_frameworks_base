bd9ff868c240f0745cb1551c3cb98687b5851356
SuperDroidBond <superdroidbond@yahoo.com>
2019-04-14 15:12:54 +0530

    CustomTextClock : User Selective Size & Color for Q Clock [1/2]
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4335,6 +4335,13 @@ public final class Settings {
         public static final String LOCKSCREEN_INDICATION_TEXT_COLOR = "lockscreen_indication_text_color";
 
         /**
+         * Change font size for the lockscreen text clock widget
+         *
+         * @hide
+         */
+        public static final String CUSTOM_TEXT_CLOCK_FONT_SIZE = "custom_text_clock_font_size";
+
+        /**
          * Control music seeking with volume up/down buttons
          *
          * @hide
