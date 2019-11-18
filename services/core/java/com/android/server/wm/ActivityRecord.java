307004259fecf936a1ea090b882a7e2d759ce63f
Jake Weinstein <jake@aospa.co>
2019-11-18 22:26:23 +0100

    [CAF] Nuke Servicetracker HAL calls [squashed].
    Revert "Inform Servicetracker HAL about Service Lifecycle events"
    
    This reverts commit f99355f195f58b7b0b2a2977f38dc527c46d857a.
    
    Revert "Passing every activity state change to Servicetracker HAL."
    
    This reverts commit d1b1712d99f4a908cad3cd8a3b75c7d0382b60a4.
    
    This thing constantly spams logs and is not needed.
    
    [spezi77] Updated for 12.1
    
    Change-Id: I81457df20134fda2840f1883f8cb82d973a7ca1c
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -365,10 +365,6 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-import vendor.qti.hardware.servicetracker.V1_2.IServicetracker;
-import vendor.qti.hardware.servicetracker.V1_2.ActivityDetails;
-import vendor.qti.hardware.servicetracker.V1_2.ActivityStats;
-import vendor.qti.hardware.servicetracker.V1_2.ActivityStates;
 /**
  * An entry in the history task, representing an activity.
  */
@@ -1763,7 +1759,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         resultWho = _resultWho;
         requestCode = _reqCode;
         setState(INITIALIZING, "ActivityRecord ctor");
-        callServiceTrackeronActivityStatechange(INITIALIZING, true);
         launchFailed = false;
         stopped = false;
         delayedResume = false;
@@ -3325,7 +3320,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 // destroyed when the next activity reports idle.
                 addToStopping(false /* scheduleIdle */, false /* idleDelayed */,
                         "completeFinishing");
-                callServiceTrackeronActivityStatechange(STOPPING, true);
                 setState(STOPPING, "completeFinishing");
             } else if (addToFinishingAndWaitForIdle()) {
                 // We added this activity to the finishing list and something else is becoming
@@ -3352,7 +3346,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
      * destroying it until the next one starts.
      */
     boolean destroyIfPossible(String reason) {
-        callServiceTrackeronActivityStatechange(FINISHING, true);
         setState(FINISHING, "destroyIfPossible");
 
         // Make sure the record is cleaned out of other places.
@@ -3404,7 +3397,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
     @VisibleForTesting
     boolean addToFinishingAndWaitForIdle() {
         ProtoLog.v(WM_DEBUG_STATES, "Enqueueing pending finish: %s", this);
-        callServiceTrackeronActivityStatechange(FINISHING, true);
         setState(FINISHING, "addToFinishingAndWaitForIdle");
         if (!mTaskSupervisor.mFinishingActivities.contains(this)) {
             mTaskSupervisor.mFinishingActivities.add(this);
@@ -3475,14 +3467,12 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             // we are not removing it from the list.
             if (finishing && !skipDestroy) {
                 ProtoLog.v(WM_DEBUG_STATES, "Moving to DESTROYING: %s (destroy requested)", this);
-                callServiceTrackeronActivityStatechange(DESTROYING, true);
                 setState(DESTROYING,
                         "destroyActivityLocked. finishing and not skipping destroy");
                 mAtmService.mH.postDelayed(mDestroyTimeoutRunnable, DESTROY_TIMEOUT);
             } else {
                 ProtoLog.v(WM_DEBUG_STATES, "Moving to DESTROYED: %s "
                         + "(destroy skipped)", this);
-                callServiceTrackeronActivityStatechange(DESTROYED, true);
                 setState(DESTROYED,
                         "destroyActivityLocked. not finishing or skipping destroy");
                 if (DEBUG_APP) Slog.v(TAG_APP, "Clearing app during destroy for activity " + this);
@@ -3495,7 +3485,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 removedFromHistory = true;
             } else {
                 ProtoLog.v(WM_DEBUG_STATES, "Moving to DESTROYED: %s (no app)", this);
-                callServiceTrackeronActivityStatechange(DESTROYED, true);
                 setState(DESTROYED, "destroyActivityLocked. not finishing and had no app");
             }
         }
@@ -3532,7 +3521,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         removeTimeouts();
         ProtoLog.v(WM_DEBUG_STATES, "Moving to DESTROYED: %s (removed from history)",
                 this);
-        callServiceTrackeronActivityStatechange(DESTROYED, true);
         setState(DESTROYED, "removeFromHistory");
         if (DEBUG_APP) Slog.v(TAG_APP, "Clearing app during remove for activity " + this);
         detachFromProcess();
@@ -3611,7 +3599,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         frozenBeforeDestroy = false;
 
         if (setState) {
-            callServiceTrackeronActivityStatechange(DESTROYED, true);
             setState(DESTROYED, "cleanUp");
             if (DEBUG_APP) Slog.v(TAG_APP, "Clearing app during cleanUp for activity " + this);
             detachFromProcess();
@@ -5153,8 +5140,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
         mState = state;
 
-        callServiceTrackeronActivityStatechange(state, false);
-
         if (getTaskFragment() != null) {
             getTaskFragment().onActivityStateChanged(this, state, reason);
         }
@@ -5214,76 +5199,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 }
                 break;
         }
-    }
-
-    void callServiceTrackeronActivityStatechange(State state, boolean early_notify) {
-        IServicetracker mServicetracker;
-        ActivityDetails aDetails = new ActivityDetails();
-        ActivityStats aStats = new ActivityStats();
-        int aState = ActivityStates.UNKNOWN;
-
-        aDetails.launchedFromPid = this.launchedFromPid;
-        aDetails.launchedFromUid = this.launchedFromUid;
-        aDetails.packageName = this.packageName;
-        aDetails.processName = (this.processName!= null)? this.processName:"none";
-        aDetails.intent = this.intent.getComponent().toString();
-        aDetails.className = this.intent.getComponent().getClassName();
-        aDetails.versioncode = this.info.applicationInfo.versionCode;
-
-        aStats.createTime = this.createTime;
-        aStats.lastVisibleTime = this.lastVisibleTime;
-        aStats.launchCount = this.launchCount;
-        aStats.lastLaunchTime = this.lastLaunchTime;
-
-        switch(state) {
-            case INITIALIZING :
-                aState = ActivityStates.INITIALIZING;
-                break;
-            case STARTED :
-                aState = ActivityStates.STARTED;
-                break;
-            case RESUMED :
-                aState = ActivityStates.RESUMED;
-                break;
-            case PAUSING :
-                aState = ActivityStates.PAUSING;
-                break;
-            case PAUSED :
-                aState = ActivityStates.PAUSED;
-                break;
-            case STOPPING :
-                aState = ActivityStates.STOPPING;
-                break;
-            case STOPPED:
-                aState = ActivityStates.STOPPED;
-                break;
-            case FINISHING :
-                aState = ActivityStates.FINISHING;
-                break;
-            case DESTROYING:
-                aState = ActivityStates.DESTROYING;
-                break;
-            case DESTROYED :
-                aState = ActivityStates.DESTROYED;
-                break;
-            case RESTARTING_PROCESS:
-                aState = ActivityStates.RESTARTING_PROCESS;
-                break;
-        }
-        if(DEBUG_SERVICETRACKER) {
-            Slog.v(TAG, "Calling mServicetracker.OnActivityStateChange with flag " + early_notify + " state " + state);
-        }
-        try {
-            mServicetracker = mAtmService.mTaskSupervisor.getServicetrackerInstance();
-            if (mServicetracker != null)
-                mServicetracker.OnActivityStateChange(aState, aDetails, aStats, early_notify);
-            else
-                Slog.e(TAG, "Unable to get servicetracker HAL instance");
-        } catch (RemoteException e) {
-                Slog.e(TAG, "Failed to send activity state change details to servicetracker HAL", e);
-                mAtmService.mTaskSupervisor.destroyServicetrackerInstance();
-        }
-
     }
 
     State getState() {
@@ -5581,7 +5496,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             }
             // An activity must be in the {@link PAUSING} state for the system to validate
             // the move to {@link PAUSED}.
-            callServiceTrackeronActivityStatechange(PAUSING, true);
             setState(PAUSING, "makeActiveIfNeeded");
             try {
                 mAtmService.getLifecycleManager().scheduleTransaction(app.getThread(), appToken,
@@ -5594,7 +5508,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             if (DEBUG_VISIBILITY) {
                 Slog.v(TAG_VISIBILITY, "Start visible activity, " + this);
             }
-            callServiceTrackeronActivityStatechange(STARTED, true);
             setState(STARTED, "makeActiveIfNeeded");
 
             try {
@@ -5813,7 +5726,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                         shortComponentName, pausingActivity != null
                                 ? pausingActivity.shortComponentName : "(none)");
                 if (isState(PAUSING)) {
-                    callServiceTrackeronActivityStatechange(PAUSED, true);
                     setState(PAUSED, "activityPausedLocked");
                     if (finishing) {
                         ProtoLog.v(WM_DEBUG_STATES,
@@ -5885,8 +5797,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         try {
             stopped = false;
             ProtoLog.v(WM_DEBUG_STATES, "Moving to STOPPING: %s (stop requested)", this);
-            callServiceTrackeronActivityStatechange(STOPPING, true);
-
             setState(STOPPING, "stopIfPossible");
             getRootTask().onARStopTriggered(this);
             if (DEBUG_VISIBILITY) {
@@ -5905,7 +5815,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             // Just in case, assume it to be stopped.
             stopped = true;
             ProtoLog.v(WM_DEBUG_STATES, "Stop failed; moving to STOPPED: %s", this);
-            callServiceTrackeronActivityStatechange(STOPPED, true);
             setState(STOPPED, "stopIfPossible");
             if (deferRelaunchUntilPaused) {
                 destroyImmediately("stop-except");
@@ -5939,7 +5848,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             removeStopTimeout();
             stopped = true;
             if (isStopping) {
-                callServiceTrackeronActivityStatechange(STOPPED, true);
                 setState(STOPPED, "activityStoppedLocked");
             }
 
@@ -8786,7 +8694,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
             mAtmService.getAppWarningsLocked().onResumeActivity(this);
         } else {
             removePauseTimeout();
-            callServiceTrackeronActivityStatechange(PAUSED, true);
             setState(PAUSED, "relaunchActivityLocked");
         }
 
@@ -8816,7 +8723,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         }
 
         // The restarting state avoids removing this record when process is died.
-        callServiceTrackeronActivityStatechange(RESTARTING_PROCESS, true);
         setState(RESTARTING_PROCESS, "restartActivityProcess");
 
         if (!mVisibleRequested || mHaveState) {
