omnirom/android_frameworks_base_old

[1/3] base: double tap to sleep on statusbar
Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -880,5 +880,16 @@ public class StatusBarWindowView extends FrameLayout {
         }
     };
 
+    public void updateSettings() {
+        boolean doubleTapToSleepEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_DOUBLE_TAP_SLEEP_GESTURE, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mNotificationPanel != null) {
+            mNotificationPanel.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+        if (mDragDownHelper != null) {
+            mDragDownHelper.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+    }
 }
 

pixeldust-project-caf/android_frameworks_base

Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -890,5 +890,16 @@ public class StatusBarWindowView extends FrameLayout {
         }
     };
 
+    public void updateSettings() {
+        boolean doubleTapToSleepEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mNotificationPanel != null) {
+            mNotificationPanel.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+        if (mDragDownHelper != null) {
+            mDragDownHelper.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+    }
 }
 