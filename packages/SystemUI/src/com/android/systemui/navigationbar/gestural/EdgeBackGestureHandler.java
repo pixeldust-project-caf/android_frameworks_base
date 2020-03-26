d7176ebf70ca295d52a8746d85fbfdf5d1b9cb49
DennySPB <dennyspb@gmail.com>
2020-03-26 11:50:37 +0300

    LongSwipeGestures: Vibrate on action if gesture haptic feedback enabled
    Change-Id: I4a17649320789497490ea4a2b6681dc51760d3b0
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -971,6 +971,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         final MotionEvent ev = MotionEvent.obtain(now, now,
                 MotionEvent.ACTION_CANCEL, 0.0f, 0.0f, 0);
         cancelGesture(ev);
+        if (mEdgeHapticEnabled) {
+            vibrateTick();
+        }
     }
 
     private void triggerAction(boolean isVertical) {
