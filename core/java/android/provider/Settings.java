cce628de2f055b36e4b564869ae6e8cc90d59b10
Pranav Vashi <neobuddy89@gmail.com>
2020-04-19 22:46:05 +0530

    Fix up custom rounded corner setting for status bar
    * It was partially broken before this patch.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9974,6 +9974,7 @@ public final class Settings {
 
         /**
          * Setting to allow setting rounded corner size and content padding
+         * @hide
          */
         public static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
         public static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
