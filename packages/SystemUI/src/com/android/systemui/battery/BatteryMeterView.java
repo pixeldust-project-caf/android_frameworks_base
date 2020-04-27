afbcc51f4386fec211e4713555b19f1aba45046a
Nicolas Dhouailly <ndhouailly%2540gmail.com>
2020-04-27 18:55:56 +0200

    Battery Styles: Remove unneeded function parameter in BatteryMeterView
    - Previously, we added a boolean parameter in the updateShowPercent
    function that was used for the battery percentage in the quick status bar header.
    With the new battery style code, this boolean is no longer needed.
    Let's remove all the code that was affected by this change.
    
    Change-Id: I29a1b894253a67812dcd8e4c0399a0ebe837ff8a
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -226,7 +226,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mThemedDrawable.setCharging(mCharging);
             mCircleDrawable.setCharging(mCharging);
             mFullCircleDrawable.setCharging(mCharging);
-            updateShowPercent(false);
+            updateShowPercent();
         } else {
             updatePercentText();
         }
@@ -236,7 +236,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mThemedDrawable.setPowerSaveEnabled(isPowerSave);
         mCircleDrawable.setPowerSaveEnabled(isPowerSave);
         mFullCircleDrawable.setPowerSaveEnabled(isPowerSave);
-        updateShowPercent(false);
+        updateShowPercent();
     }
 
     private TextView loadPercentView() {
@@ -335,7 +335,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                     mBatteryPercentView.setTextAppearance(mPercentageStyleId);
                 }
                 if (mTextColor != 0) mBatteryPercentView.setTextColor(mTextColor);
-                updatePercentText();
                 addView(mBatteryPercentView,
                         new ViewGroup.LayoutParams(
                                 LayoutParams.WRAP_CONTENT,
