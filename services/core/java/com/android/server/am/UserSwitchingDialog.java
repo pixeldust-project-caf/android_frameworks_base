2670595f50eab9bceadf43deeae577429293adb9
spezi77 <spezi7713@gmx.net>
2020-05-17 11:33:05 +0200

    Turn off debug msg for user switching.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I476681c6411a13b253bb4d186c5b3579c35187c6

--- services/core/java/com/android/server/am/UserSwitchingDialog.java
+++ services/core/java/com/android/server/am/UserSwitchingDialog.java
@@ -47,8 +47,7 @@ class UserSwitchingDialog extends AlertDialog
     // Time to wait for the onWindowShown() callback before continuing the user switch
     private static final int WINDOW_SHOWN_TIMEOUT_MS = 3000;
 
-    // User switching doesn't happen that frequently, so it doesn't hurt to have it always on
-    protected static final boolean DEBUG = true;
+    protected static final boolean DEBUG = false;
 
     private final ActivityManagerService mService;
     private final int mUserId;
