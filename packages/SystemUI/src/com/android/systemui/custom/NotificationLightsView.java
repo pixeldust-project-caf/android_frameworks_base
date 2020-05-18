AOSiP/platform_frameworks_base

SystemUI: add Pulse and Ambient notification bars [1/2]
Add Pulse and Ambient Display bars for notifications

Change-Id: Ie4f1730bb15d0cd4db7ceb3a0281c114707d3789

packages/SystemUI/src/com/android/systemui/omni/NotificationLightsView.java
@@ -1,0 +1,128 @@
+/*
+* Copyright (C) 2019 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.omni;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.R;
+
+public class NotificationLightsView extends RelativeLayout {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "NotificationLightsView";
+    private View mNotificationAnimView;
+    private ValueAnimator mLightAnimator;
+    private boolean mPulsing;
+    private boolean mNotification;
+
+    public NotificationLightsView(Context context) {
+        this(context, null);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        if (DEBUG) Log.d(TAG, "new");
+    }
+
+    private Runnable mLightUpdate = new Runnable() {
+        @Override
+        public void run() {
+            if (DEBUG) Log.d(TAG, "run");
+            animateNotification(mNotification);
+        }
+    };
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (DEBUG) Log.d(TAG, "draw");
+        //post(mLightUpdate);
+    }
+
+    public void setPulsing(boolean pulsing) {
+        if (mPulsing == pulsing) {
+            return;
+        }
+        mPulsing = pulsing;
+    }
+
+    public void animateNotification(boolean mNotification) {
+        int defaultColor = Color.parseColor("#3980FF");
+        /*int color = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_PULSE_AMBIANT_LIGHT_COLOR, 0x3980FF,
+                UserHandle.USER_CURRENT);*/
+        //int color = Color.parseColor("#3980FF");
+        int color = defaultColor;
+        StringBuilder sb = new StringBuilder();
+        sb.append("animateNotification color ");
+        sb.append(Integer.toHexString(color));
+        if (DEBUG) Log.d(TAG, sb.toString());
+        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
+        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
+        leftView.setColorFilter(color);
+        rightView.setColorFilter(color);
+        mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
+        mLightAnimator.setDuration(2000);
+        //Infinite animation only on Always On Notifications
+        if (mNotification) {
+            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        }
+        mLightAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (DEBUG) Log.d(TAG, "onAnimationUpdate");
+                float progress = ((Float) animation.getAnimatedValue()).floatValue();
+                leftView.setScaleY(progress);
+                rightView.setScaleY(progress);
+                float alpha = 1.0f;
+                if (progress <= 0.3f) {
+                    alpha = progress / 0.3f;
+                } else if (progress >= 1.0f) {
+                    alpha = 2.0f - progress;
+                }
+                leftView.setAlpha(alpha);
+                rightView.setAlpha(alpha);
+            }
+        });
+        if (DEBUG) Log.d(TAG, "start");
+        mLightAnimator.start();
+    }
+
+}

services/core/java/com/android/server/power/PowerManagerService.java
@@ -854,6 +854,8 @@ public final class PowerManagerService extends SystemService
         // set initial value
         Settings.System.putIntForUser(mContext.getContentResolver(),
                 Settings.System.DOZE_ON_CHARGE_NOW, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0, UserHandle.USER_CURRENT);
 
         synchronized (mLock) {
             mSystemReady = true;
@@ -968,6 +970,13 @@ public final class PowerManagerService extends SystemService
         resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.DOZE_ON_CHARGE),
                 false, mSettingsObserver, UserHandle.USER_ALL);
+        resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT),
+                false, mSettingsObserver, UserHandle.USER_ALL);
+        resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED),
+                false, mSettingsObserver, UserHandle.USER_ALL);
+
         IVrManager vrManager = IVrManager.Stub.asInterface(getBinderService(Context.VR_SERVICE));
         if (vrManager != null) {
             try {
@@ -1064,9 +1073,25 @@ public final class PowerManagerService extends SystemService
                 Settings.Global.STAY_ON_WHILE_PLUGGED_IN, BatteryManager.BATTERY_PLUGGED_AC);
         mTheaterModeEnabled = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.THEATER_MODE_ON, 0) == 1;
-        mAlwaysOnEnabled = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
         mDozeOnChargeEnabled = Settings.System.getIntForUser(resolver,
                 Settings.System.DOZE_ON_CHARGE, 0, UserHandle.USER_CURRENT) != 0;
+        boolean mAmbientLights = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED,
+                0, UserHandle.USER_CURRENT) != 0;
+        if (mAmbientLights) {
+            boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
+                    Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                     Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, dozeOnNotification ? 1 : 0,
+                     UserHandle.USER_CURRENT);
+        } else {
+             Settings.System.putIntForUser(mContext.getContentResolver(),
+                     Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0,
+                     UserHandle.USER_CURRENT);
+        }
+        // depends on OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED - so MUST be afterwards
+        // no need to call us again
+        mAlwaysOnEnabled = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
 
         if (mSupportsDoubleTapWakeConfig) {
             boolean doubleTapWakeEnabled = Settings.Secure.getIntForUser(resolver,

pixeldust-project-caf/android_frameworks_base

Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SystemUI/src/com/android/systemui/custom/NotificationLightsView.java
@@ -1,0 +1,128 @@
+/*
+* Copyright (C) 2019 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.custom;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.R;
+
+public class NotificationLightsView extends RelativeLayout {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "NotificationLightsView";
+    private View mNotificationAnimView;
+    private ValueAnimator mLightAnimator;
+    private boolean mPulsing;
+    private boolean mNotification;
+
+    public NotificationLightsView(Context context) {
+        this(context, null);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        if (DEBUG) Log.d(TAG, "new");
+    }
+
+    private Runnable mLightUpdate = new Runnable() {
+        @Override
+        public void run() {
+            if (DEBUG) Log.d(TAG, "run");
+            animateNotification(mNotification);
+        }
+    };
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (DEBUG) Log.d(TAG, "draw");
+        //post(mLightUpdate);
+    }
+
+    public void setPulsing(boolean pulsing) {
+        if (mPulsing == pulsing) {
+            return;
+        }
+        mPulsing = pulsing;
+    }
+
+    public void animateNotification(boolean mNotification) {
+        int defaultColor = Color.parseColor("#3980FF");
+        /*int color = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIANT_LIGHT_COLOR, 0x3980FF,
+                UserHandle.USER_CURRENT);*/
+        //int color = Color.parseColor("#3980FF");
+        int color = defaultColor;
+        StringBuilder sb = new StringBuilder();
+        sb.append("animateNotification color ");
+        sb.append(Integer.toHexString(color));
+        if (DEBUG) Log.d(TAG, sb.toString());
+        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
+        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
+        leftView.setColorFilter(color);
+        rightView.setColorFilter(color);
+        mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
+        mLightAnimator.setDuration(2000);
+        //Infinite animation only on Always On Notifications
+        if (mNotification) {
+            mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        }
+        mLightAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (DEBUG) Log.d(TAG, "onAnimationUpdate");
+                float progress = ((Float) animation.getAnimatedValue()).floatValue();
+                leftView.setScaleY(progress);
+                rightView.setScaleY(progress);
+                float alpha = 1.0f;
+                if (progress <= 0.3f) {
+                    alpha = progress / 0.3f;
+                } else if (progress >= 1.0f) {
+                    alpha = 2.0f - progress;
+                }
+                leftView.setAlpha(alpha);
+                rightView.setAlpha(alpha);
+            }
+        });
+        if (DEBUG) Log.d(TAG, "start");
+        mLightAnimator.start();
+    }
+
+}

services/core/java/com/android/server/power/PowerManagerService.java
@@ -862,6 +862,9 @@ public final class PowerManagerService extends SystemService
     }
 
     public void systemReady(IAppOpsService appOps) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0, UserHandle.USER_CURRENT);
+
         synchronized (mLock) {
             mSystemReady = true;
             mAppOps = appOps;
@@ -984,6 +987,12 @@ public final class PowerManagerService extends SystemService
         resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.SMART_CHARGING_RESET_STATS),
                 false, mSettingsObserver, UserHandle.USER_ALL);
+        resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.AMBIENT_NOTIFICATION_LIGHT),
+                false, mSettingsObserver, UserHandle.USER_ALL);
+        resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED),
+                false, mSettingsObserver, UserHandle.USER_ALL);
 
         IVrManager vrManager = IVrManager.Stub.asInterface(getBinderService(Context.VR_SERVICE));
         if (vrManager != null) {
@@ -1094,7 +1103,6 @@ public final class PowerManagerService extends SystemService
                 Settings.Global.STAY_ON_WHILE_PLUGGED_IN, BatteryManager.BATTERY_PLUGGED_AC);
         mTheaterModeEnabled = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.THEATER_MODE_ON, 0) == 1;
-        mAlwaysOnEnabled = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
         mSmartChargingEnabled = Settings.System.getInt(resolver,
                 Settings.System.SMART_CHARGING, 0) == 1;
         mSmartChargingLevel = Settings.System.getInt(resolver,
@@ -1105,6 +1113,23 @@ public final class PowerManagerService extends SystemService
                 mSmartChargingResumeLevelDefaultConfig);
         mSmartChargingResetStats = Settings.System.getInt(resolver,
                 Settings.System.SMART_CHARGING_RESET_STATS, 0) == 1;
+        boolean mAmbientLights = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
+                0, UserHandle.USER_CURRENT) != 0;
+        if (mAmbientLights) {
+            boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
+                    Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                     Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, dozeOnNotification ? 1 : 0,
+                     UserHandle.USER_CURRENT);
+        } else {
+             Settings.System.putIntForUser(mContext.getContentResolver(),
+                     Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0,
+                     UserHandle.USER_CURRENT);
+        }
+        // depends on AMBIENT_NOTIFICATION_LIGHT_ACTIVATED - so MUST be afterwards
+        // no need to call us again
+        mAlwaysOnEnabled = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
 
         if (mSupportsDoubleTapWakeConfig) {
             boolean doubleTapWakeEnabled = Settings.Secure.getIntForUser(resolver,