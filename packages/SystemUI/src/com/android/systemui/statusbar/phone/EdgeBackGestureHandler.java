ezio84/abc-n_frameworks_base

Move launchCamera and launchVoiceSearch actions to custom Utils [1/2]

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -38,7 +38,6 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -585,10 +584,10 @@ public class EdgeBackGestureHandler implements DisplayListener {
                 mAssistManager.startAssist(new Bundle() /* args */);
                 break;
             case 2: // Voice search
-                launchVoiceSearch(mContext);
+                Utils.launchVoiceSearch(mContext);
                 break;
             case 3: // Camera
-                launchCamera(mContext);
+                Utils.launchCamera(mContext);
                 break;
             case 4: // Flashlight
                 Utils.toggleCameraFlash();
@@ -642,18 +641,6 @@ public class EdgeBackGestureHandler implements DisplayListener {
             context.startActivity(intent);
         } catch (Exception e) {
         }
-    }
-
-    private static void launchCamera(Context context) {
-        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        context.startActivity(intent);
-    }
-
-    private void launchVoiceSearch(Context context) {
-        Intent intent = new Intent(Intent.ACTION_SEARCH_LONG_PRESS);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
     }
 
     private void updateEdgePanelPosition(float touchY) {

pixeldust-project-caf/android_frameworks_base

Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -38,7 +38,6 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -584,10 +583,10 @@ public class EdgeBackGestureHandler implements DisplayListener {
                 mAssistManager.startAssist(new Bundle() /* args */);
                 break;
             case 2: // Voice search
-                launchVoiceSearch(mContext);
+                PixeldustUtils.launchVoiceSearch(mContext);
                 break;
             case 3: // Camera
-                launchCamera(mContext);
+                PixeldustUtils.launchCamera(mContext);
                 break;
             case 4: // Flashlight
                 PixeldustUtils.toggleCameraFlash();
@@ -641,18 +640,6 @@ public class EdgeBackGestureHandler implements DisplayListener {
             context.startActivity(intent);
         } catch (Exception e) {
         }
-    }
-
-    private static void launchCamera(Context context) {
-        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        context.startActivity(intent);
-    }
-
-    private void launchVoiceSearch(Context context) {
-        Intent intent = new Intent(Intent.ACTION_SEARCH_LONG_PRESS);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
     }
 
     private void updateEdgePanelPosition(float touchY) {