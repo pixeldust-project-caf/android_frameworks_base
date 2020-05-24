f345d052d39b331411a6dd834fb6442ae5c22378
Joe Maples <joe@maples.dev>
2020-05-24 10:58:49 +0200

    Port ambient Now Playing container from Pixel [stripped down version]
    ezio84: this is a very stripped down part of code from the original commit by Joe:
    "SystemUI: Port Now Playing feature from Pixel [1/2]"
    https://review.carbonrom.org/#/c/4950/
    adapted to be called from StatusBar class for our local Ambient Display
    music ticker.
    Removed everything we don't need, services and listeners, and reparsed
    everything to make it clean.
    
    [spezi77] edits:
    * Cleaned up for Android 11.
    * Updated for Android 13.
    
    Change-Id: If1c2cf233d72ed8b31bc44f9852ba7b5c1181697
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -135,6 +135,7 @@ import com.android.systemui.R;
 import com.android.systemui.accessibility.floatingmenu.AccessibilityFloatingMenuController;
 import com.android.systemui.animation.ActivityLaunchAnimator;
 import com.android.systemui.animation.DelegateLaunchAnimatorController;
+import com.android.systemui.ambientmusic.AmbientIndicationContainer;
 import com.android.systemui.assist.AssistManager;
 import com.android.systemui.biometrics.AuthRippleController;
 import com.android.systemui.broadcast.BroadcastDispatcher;
@@ -1223,6 +1224,9 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
         mAmbientIndicationContainer = mNotificationShadeWindowView.findViewById(
                 R.id.ambient_indication_container);
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer) mAmbientIndicationContainer).initializeView(this);
+        }
 
         mAutoHideController.setStatusBar(new AutoHideUiElement() {
             @Override
