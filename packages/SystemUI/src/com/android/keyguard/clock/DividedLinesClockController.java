2d7e9fdb71cdc183eeacce2e441ab6609875f820
Pranav Vashi <neobuddy89@gmail.com>
2020-05-31 20:00:26 +0530

    DividerLines Clock: Set 24-hr time format as well
    * Also, clean up unused mDate.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ib27fbbb8ada2ab7b7a16f5e2a6e6530cf82fb665
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/keyguard/clock/DividedLinesClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/DividedLinesClockController.java
@@ -77,11 +77,6 @@ public class DividedLinesClockController implements ClockPlugin {
     private TextClock mClock;
 
     /**
-     * Text date in preview view hierarchy.
-     */
-    private TextClock mDate;
-
-    /**
      * Top and bottom dividers in preview view hierarchy.
      */
     private View mTopLine;
@@ -121,8 +116,6 @@ public class DividedLinesClockController implements ClockPlugin {
         mView = (ClockLayout) mLayoutInflater
                 .inflate(R.layout.divided_lines_clock, null);
         mClock = mView.findViewById(R.id.clock);
-        mDate = mView.findViewById(R.id.date);
-        mClock.setFormat12Hour("h:mm");
         onTimeTick();
     }
 
@@ -130,7 +123,6 @@ public class DividedLinesClockController implements ClockPlugin {
     public void onDestroyView() {
         mView = null;
         mClock = null;
-        mDate = null;
         mTopLine = null;
         mBottomLine = null;
     }
