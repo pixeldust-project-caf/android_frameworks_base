c40328cdecd64b95be979c3035d7439844fe44a0
spezi77 <spezi7713@gmx.net>
2020-06-14 12:22:30 +0200

    KeyguardSliceProvider: Don't show song name if TypeClock is in use. Otherwise the layout would be broken
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I826a927b06d343bbf683f251a3c99cdd00553196

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -231,13 +231,16 @@ public class KeyguardSliceProvider extends SliceProvider implements
     public boolean needsMediaLocked() {
         boolean keepWhenAwake = mKeyguardBypassController != null
                 && mKeyguardBypassController.getBypassEnabled() && mDozeParameters.getAlwaysOn();
-        boolean isCenterMusicTickerEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+        String currentClock = Settings.Secure.getString(
+                mContentResolver, Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE);
+        boolean isTypeClockSelected = currentClock == null ? false : currentClock.contains("Type");
+        boolean isCenterMusicTickerEnabled = Settings.System.getIntForUser(mContentResolver,
                 Settings.System.FORCE_AMBIENT_FOR_MEDIA, 1, UserHandle.USER_CURRENT) == 2;
         // Show header if music is playing and the status bar is in the shade state. This way, an
         // animation isn't necessary when pressing power and transitioning to AOD.
         boolean keepWhenShade = mStatusBarState == StatusBarState.SHADE && mMediaIsVisible;
         return !TextUtils.isEmpty(mMediaTitle) && mMediaIsVisible && (mDozing || keepWhenAwake
-                || keepWhenShade) && isCenterMusicTickerEnabled;
+                || keepWhenShade) && isCenterMusicTickerEnabled && !isTypeClockSelected;
     }
 
     protected void addMediaLocked(ListBuilder listBuilder) {
