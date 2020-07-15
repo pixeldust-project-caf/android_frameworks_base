79d05f1b4646f705e1c6da62e3f48c7297737a70
spezi77 <spezi7713@gmx.net>
2020-07-15 13:01:23 +0200

    Revert "base: SystemUI: add OmniRecord based qs tile"
    This reverts commit e061f3adabd6fccdf7dfe7df3b4a8ef74edaa375.
    
    Change-Id: Ie5bb70cd5146c160f96c2df5ba05d2dc23eca070

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,7 +49,6 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
-import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -93,7 +92,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<ScreenshotTile> mScreenshotTileProvider;
     private final Provider<HWKeysTile> mHWKeysTileProvider;
-    private final Provider<ScreenrecordTile> mScrTileProvider;
     private final Provider<LocaleTile> mLocaleTileProvider;
     private final Provider<AlwaysOnDisplayTile> mAlwaysOnDisplayTileProvider;
     private final Provider<ImmersiveTile> mImmersiveTileProvider;
@@ -129,7 +127,6 @@ public class QSFactoryImpl implements QSFactory {
             Provider<CaffeineTile> caffeineTileProvider,
             Provider<ScreenshotTile> screenshotTileProvider,
             Provider<HWKeysTile> hWKeysTileProvider,
-            Provider<ScreenrecordTile> scrTileProvider,
             Provider<LocaleTile> localeTileProvider,
             Provider<AlwaysOnDisplayTile> alwaysOnDisplayTileProvider,
             Provider<ImmersiveTile> immersiveTileProvider,
@@ -161,7 +158,6 @@ public class QSFactoryImpl implements QSFactory {
         mCaffeineTileProvider = caffeineTileProvider;
         mScreenshotTileProvider = screenshotTileProvider;
         mHWKeysTileProvider = hWKeysTileProvider;
-        mScrTileProvider = scrTileProvider;
         mLocaleTileProvider = localeTileProvider;
         mAlwaysOnDisplayTileProvider = alwaysOnDisplayTileProvider;
         mImmersiveTileProvider = immersiveTileProvider;
@@ -233,8 +229,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mScreenshotTileProvider.get();
             case "hwkeys":
                 return mHWKeysTileProvider.get();
-            case "scr":
-                return mScrTileProvider.get();
             case "locale":
                 return mLocaleTileProvider.get();
             case "always_on_display":
