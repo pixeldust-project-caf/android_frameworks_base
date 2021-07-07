8bfb82df5d270799d55e2b45c7166b0d5d41d393
maxwen <max.weninger@gmail.com>
2021-07-07 20:45:51 +0530

    SystemUI: Hide status bar on lock screen [1/2]
    Taken from Omni ROM FWB-
    omnirom/android_frameworks_base@0b833a0
    
    Source - omnirom/android_frameworks_base@0b833a0
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4902,6 +4902,9 @@ public class NotificationPanelViewController extends PanelViewController {
                             /* alpha= */ 1f,
                             keyguardShowing ? View.VISIBLE : View.INVISIBLE);
                 }
+                if (keyguardShowing) {
+                    mKeyguardStatusBar.toggleContents(true);
+                }
                 if (keyguardShowing && oldState != mBarState) {
                     if (mQs != null) {
                         mQs.hideImmediately();
