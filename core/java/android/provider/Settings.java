660830196ad16a31786a620b4ece891ca3c2ed9c
maxwen <max.weninger@gmail.com>
2021-07-07 20:45:51 +0530

    SystemUI: Hide status bar on lock screen [1/2]
    Taken from Omni ROM FWB-
    omnirom/android_frameworks_base@0b833a0
    
    Source - omnirom/android_frameworks_base@0b833a0
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5581,6 +5581,12 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Hide status bar on lock screen
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_STATUS_BAR = "hide_lockscreen_status_bar";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
