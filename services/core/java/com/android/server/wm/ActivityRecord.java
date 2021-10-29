c6b59220bee783f32f4c1766c999c6893df8d94c
wangchende <wangchende@allwinnertech.com>
2021-10-29 17:08:55 +0800

    Fix: if anrApp is null, Null pointer exception occurred, Anr trace capture failed
    Bug: 204520514
    Change-Id: I9cef64418ff9d24c884b513c672185e924dd8817
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -6478,7 +6478,8 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         }
 
         if (windowFromSameProcessAsActivity) {
-            return mAtmService.mAmInternal.inputDispatchingTimedOut(anrApp.mOwner,
+            return mAtmService.mAmInternal.inputDispatchingTimedOut(
+                    anrApp != null? anrApp.mOwner : null,
                     anrActivity.shortComponentName, anrActivity.info.applicationInfo,
                     shortComponentName, app, false, reason);
         } else {
