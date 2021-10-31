87fad73e162d40ab2d7b8fb00e293aa92b2c34fd
SagarMakhar <sagarmakhar@gmail.com>
2021-10-31 18:39:26 +0000

    Allow hiding call strength icons
    [spezi77] Merged toggle into a one commit
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/tuner/StatusbarItems.java
+++ packages/SystemUI/src/com/android/systemui/tuner/StatusbarItems.java
@@ -14,14 +14,38 @@
 
 package com.android.systemui.tuner;
 
+import android.content.Context;
+import android.provider.Settings;
 import android.os.Bundle;
+import android.os.UserHandle;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
 import com.android.systemui.R;
 
 public class StatusbarItems extends PreferenceFragment {
 
+    private static final String HIDE_QS_CALL_STRENGTH = "hide_qs_call_strength";
+
+    private SwitchPreference mCallStrengthIcon;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.statusbar_items);
+
+        mCallStrengthIcon = (SwitchPreference) findPreference(HIDE_QS_CALL_STRENGTH);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mCallStrengthIcon) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    HIDE_QS_CALL_STRENGTH, checked ? 0 : 1);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 }
