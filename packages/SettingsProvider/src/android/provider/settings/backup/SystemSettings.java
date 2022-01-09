5dcafbe133213a489636f8fdcd3a2bd4a6067965
maxwen <max.weninger@gmail.com>
2022-01-09 17:39:28 +0100

    [1/2] SystemUI: qs hide label setting
    [spezi77] Combined into a single commit & adapt for SettingsObserver
    
    Change-Id: Icf697b7251aed37a77630cdca906a9912f1d8ea2
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.OMNI_QS_TILE_VERTICAL_LAYOUT,
         Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE,
         Settings.System.OMNI_QS_LAYOUT_COLUMNS,
+        Settings.System.OMNI_QS_TILE_LABEL_HIDE,
     };
 }
