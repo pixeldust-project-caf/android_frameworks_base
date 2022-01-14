b8e5b55acaedd38398c30bd99fd0e5db51c259c7
spezi77 <spezi7713@gmx.net>
2022-01-14 16:51:09 +0100

    Revert "Keyguard/UI: Fix status bar / quick settings margins and paddings"
    This reverts commit 48e93a7c0d7287b53f753807f3372bf67a4fc9c2.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I7a9e7984454dbb1f43052f640a5e0e6ad3a79a69

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -480,9 +480,7 @@ public class QuickStatusBarHeader extends FrameLayout implements
                 StatusBarWindowView.paddingNeededForCutoutAndRoundedCorner(
                         cutout, cornerCutoutPadding, -1);
         mDatePrivacyView.setPadding(padding.first, 0, padding.second, 0);
-        if (cutout == null) {
-            mDateContainer.setPadding(0, 0, 0, 0);
-        }
+        //mClockIconsView.setPadding(padding.first, 0, padding.second, 0);
         LinearLayout.LayoutParams datePrivacySeparatorLayoutParams =
                 (LinearLayout.LayoutParams) mDatePrivacySeparator.getLayoutParams();
         LinearLayout.LayoutParams mClockIconsSeparatorLayoutParams =
@@ -573,6 +571,12 @@ public class QuickStatusBarHeader extends FrameLayout implements
                 mWaterfallTopInset,
                 paddingRight,
                 0);
+/*
+        mClockIconsView.setPadding(paddingLeft,
+                mWaterfallTopInset,
+                paddingRight,
+                0);
+*/
     }
 
     public void updateEverything() {
