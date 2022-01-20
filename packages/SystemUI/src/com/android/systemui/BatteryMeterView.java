7796dcedbe8a90465b0594fe76194720618d0c0c
spezi77 <spezi7713@gmx.net>
2022-01-20 18:03:15 +0100

    BatteryMeterView: Move null check to the beginning of the method. Avoid unnecessary processing time when the battery icon won't get scaled anyway.
    Following up: 96603a0
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ib8b9fca0a8cb949b47f30339e6d70dc61d2215aa

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -474,6 +474,9 @@ public class BatteryMeterView extends LinearLayout implements
      * Looks up the scale factor for status bar icons and scales the battery view by that amount.
      */
     private void scaleBatteryMeterViews() {
+        if (mBatteryIconView == null) {
+            return;
+        }
         Resources res = getContext().getResources();
         TypedValue typedValue = new TypedValue();
 
@@ -494,9 +497,7 @@ public class BatteryMeterView extends LinearLayout implements
                 (int) (batteryWidth * iconScaleFactor), (int) (batteryHeight * iconScaleFactor));
         scaledLayoutParams.setMargins(0, 0, 0, marginBottom);
 
-        if (mBatteryIconView != null) {
-            mBatteryIconView.setLayoutParams(scaledLayoutParams);
-        }
+        mBatteryIconView.setLayoutParams(scaledLayoutParams);
     }
 
     public void updateBatteryStyle() {
