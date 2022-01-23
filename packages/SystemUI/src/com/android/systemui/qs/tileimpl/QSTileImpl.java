d1ca54f1c8a9f332b9c671e9a4149cfa0ffa440b
spezi77 <spezi7713@gmx.net>
2022-01-23 11:50:54 +0100

    QuickSettings: Use tick instead of vibrate for normal tap on qs tile. * Imho the default vibration effect doesn't fit here at all. The   subtle ticking is so much more consistent with the rest of the OS.
    Inspired by commit 5619f08
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I5698b6cf882f2a7611b174651308fc67c2c59e58
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -35,9 +35,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.metrics.LogMaker;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
@@ -293,6 +295,15 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         }
     }
 
+    public void vibrateTick() {
+        if (mVibrator != null) {
+            if (mVibrator.hasVibrator()) {
+                AsyncTask.execute(() ->
+                        mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_TICK)));
+            }
+        }
+    }
+
     public void addCallback(Callback callback) {
         mHandler.obtainMessage(H.ADD_CALLBACK, callback).sendToTarget();
     }
@@ -317,7 +328,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         if (!mFalsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
             mHandler.obtainMessage(H.CLICK, eventId, 0, view).sendToTarget();
         }
-        vibrateTile(45);
+        vibrateTick();
     }
 
     public void secondaryClick(@Nullable View view) {
