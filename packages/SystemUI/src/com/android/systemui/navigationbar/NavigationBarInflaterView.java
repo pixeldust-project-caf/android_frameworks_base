a6b2b32c4d2514c066710f40df224aba208378e9
Pranav Vashi <neobuddy89@gmail.com>
2022-01-30 19:21:43 +0530

    Navbar: Fix issue where pill disappears in gestural mode
    Issue:
    - The 2 and 3 button navbar layout options were causing the gesture 'pill' to
      disappear when enabled.
    
    Test:
    - Enable 2 or 3 button navbar
    - Go into layout options
    - Set the layout to compact or any other option but 'Normal'
    - Enable the gesture navbar
    
    RCA:
    - When custom layout is set, it doesn't let new navigation mode to take effect.
    
    Solution:
    - Always reinflate layout when navigation mode changes, and then apply custom layout
      only if it's gestural navigation mode.
    
    Original issue ref:
    https://github.com/AospExtended/platform_frameworks_base/commit/dc4bca0f5a79d0bc7de815362f76d6579ca20a82
    
    Change-Id: Id4f0bc8c7c155090bb9f7ce24edd5bdbbd7553d5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -113,6 +113,7 @@ public class NavigationBarInflaterView extends FrameLayout
 
     private OverviewProxyService mOverviewProxyService;
     private int mNavBarMode = NAV_BAR_MODE_3BUTTON;
+    private String mNavBarLayout;
 
     private boolean mUsingCustomLayout;
     private boolean mInverseLayout;
@@ -195,7 +196,10 @@ public class NavigationBarInflaterView extends FrameLayout
     @Override
     public void onTuningChanged(String key, String newValue) {
         if (NAV_BAR_VIEWS.equals(key)) {
-            setNavigationBarLayout(newValue);
+            mNavBarLayout = (String) newValue;
+            if (!QuickStepContract.isGesturalMode(mNavBarMode)) {
+                setNavigationBarLayout(mNavBarLayout);
+            }
         }
         if (NAV_BAR_INVERSE.equals(key)) {
             mInverseLayout = TunerService.parseIntegerSwitch(newValue, false);
@@ -220,20 +224,15 @@ public class NavigationBarInflaterView extends FrameLayout
 
     public void setNavigationBarLayout(String layoutValue) {
         if (!Objects.equals(mCurrentLayout, layoutValue)) {
-            mUsingCustomLayout = layoutValue != null;
             clearViews();
             inflateLayout(layoutValue);
         }
     }
 
     public void onLikelyDefaultLayoutChange() {
-        // Don't override custom layouts
-        if (mUsingCustomLayout) return;
-        // Reevaluate new layout
-        final String newValue = getDefaultLayout();
-        if (!Objects.equals(mCurrentLayout, newValue)) {
-            clearViews();
-            inflateLayout(newValue);
+        setNavigationBarLayout(getDefaultLayout());
+        if (!QuickStepContract.isGesturalMode(mNavBarMode)) {
+            setNavigationBarLayout(mNavBarLayout);
         }
     }
 
