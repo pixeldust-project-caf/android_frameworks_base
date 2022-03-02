fcd57426e58380f3d1656867c9aec109c26299c6
jhonboy121 <alfredmathew05@gmail.com>
2022-03-03 00:30:45 +0530

    SystemUI: Improve mobile data style switch logic
    * adapted to A12 L
    * ditch TunerService and use ContentObserver
    * added setting key to backup array
    * move settings observer to StatusBarIconControllerImpl class
    * set style when an icon manager is added and update it whenever settings changes, this way we don't update all icons whenever addMobileIcon is called
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -104,5 +104,6 @@ public class SystemSettings {
         Settings.System.RINGTONE_VIBRATION_PATTERN,
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.SHOW_FOURG_ICON,
+        Settings.System.USE_OLD_MOBILETYPE,
     };
 }
