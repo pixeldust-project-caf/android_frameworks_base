1aef7e979ae75a28bd7e77baaf95341bf0ebfc1f
Pranav Vashi <neobuddy89@gmail.com>
2022-03-09 00:19:33 +0530

    Reload navbar icons on changing style [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I175f254ea37c1cac25a73852914239db7c0f8309

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5633,6 +5633,12 @@ public final class Settings {
         public static final String GLOBAL_ACTIONS_MAX_ROWS = "global_actions_max_rows";
 
         /**
+         * Navbar style
+         * @hide
+         */
+        public static final String NAVBAR_STYLE = "navbar_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
