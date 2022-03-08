ebb70cf29ec5f63df84bb7a775cae89903be3383
Pranav Vashi <neobuddy89@gmail.com>
2022-03-08 20:55:15 +0530

    NavigationBarInflaterView: Improve applying overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -782,7 +782,7 @@ public class NavigationBarView extends FrameLayout implements
     public void updateStates() {
         if (mNavigationInflaterView != null) {
             // Reinflate the navbar if needed, no-op unless the swipe up state changes
-            mNavigationInflaterView.onLikelyDefaultLayoutChange();
+            mNavigationInflaterView.onLikelyDefaultLayoutChange(false);
         }
 
         updateSlippery();
