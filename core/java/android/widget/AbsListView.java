59e6afb958bf68436c77dcd1e1f30d8fa2b76054
dlwlrma123 <alexfinhart@gmail.com>
2022-03-11 07:19:41 +0800

    android: responsiveness optimizations
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4814,6 +4814,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
+            mScroller.setFriction(0.005f);
         }
 
         // Use AbsListView#fling(int) instead
