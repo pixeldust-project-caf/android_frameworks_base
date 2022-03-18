1627a4730b2d0171480a83b0e4774d08cb10abb8
Daniel Micay <danielmicay@gmail.com>
2022-03-18 08:42:08 -0400

    improve PendingIntent security check compatibility
    This switches to secure-by-default instead of crash-by-default for API
    31 to work around apps which have updated to API 31 without specifying
    either FLAG_MUTABLE or FLAG_IMMUTABLE for PendingIntents. If the app
    ends up needing the FLAG_MUTABLE behavior, it may crash later, but it
    should still be obvious why it happened.
    
    There are many apps with outdated Play services client libraries lacking
    support for Android 12 which are nonetheless targeting API 31 or higher
    and will crash in certain situations. Google Play services will ask the
    client library to request runtime permissions from the user on behalf of
    it when it thinks that they're required for an operation that's
    requested. The older client libraries will cause a crash in the app by
    trying to create a PendingIntent with no FLAG_MUTABLE or FLAG_IMMUTABLE
    specified. This is a much more common issue on GrapheneOS since Play
    services is a regular user installed app with no special access or
    privileges, and starts without any standard runtime permissions granted
    to it.
    
    Signed-off-by: SKORPION29 <sayansarkar29.slg@gmail.com>
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/app/PendingIntent.java
+++ core/java/android/app/PendingIntent.java
@@ -358,7 +358,7 @@ public final class PendingIntent implements Parcelable {
         sOnMarshaledListener.set(listener);
     }
 
-    private static void checkFlags(int flags, String packageName) {
+    private static int checkFlags(int flags, String packageName) {
         final boolean flagImmutableSet = (flags & PendingIntent.FLAG_IMMUTABLE) != 0;
         final boolean flagMutableSet = (flags & PendingIntent.FLAG_MUTABLE) != 0;
 
@@ -375,8 +375,11 @@ public final class PendingIntent implements Parcelable {
                     + " using FLAG_IMMUTABLE, only use FLAG_MUTABLE if some functionality"
                     + " depends on the PendingIntent being mutable, e.g. if it needs to"
                     + " be used with inline replies or bubbles.";
-                throw new IllegalArgumentException(msg);
+            // secure-by-default instead of crash-by-default for better compatibility
+            Log.e(TAG, msg);
+            return flags | PendingIntent.FLAG_IMMUTABLE;
         }
+        return flags;
     }
 
     /**
@@ -458,7 +461,7 @@ public final class PendingIntent implements Parcelable {
             @NonNull Intent intent, int flags, Bundle options, UserHandle user) {
         String packageName = context.getPackageName();
         String resolvedType = intent.resolveTypeIfNeeded(context.getContentResolver());
-        checkFlags(flags, packageName);
+        flags = checkFlags(flags, packageName);
         try {
             intent.migrateExtraStreamToClipData(context);
             intent.prepareToLeaveProcess(context);
@@ -593,7 +596,7 @@ public final class PendingIntent implements Parcelable {
             intents[i].prepareToLeaveProcess(context);
             resolvedTypes[i] = intents[i].resolveTypeIfNeeded(context.getContentResolver());
         }
-        checkFlags(flags, packageName);
+        flags = checkFlags(flags, packageName);
         try {
             IIntentSender target =
                 ActivityManager.getService().getIntentSenderWithFeature(
@@ -645,7 +648,7 @@ public final class PendingIntent implements Parcelable {
             Intent intent, int flags, UserHandle userHandle) {
         String packageName = context.getPackageName();
         String resolvedType = intent.resolveTypeIfNeeded(context.getContentResolver());
-        checkFlags(flags, packageName);
+        flags = checkFlags(flags, packageName);
         try {
             intent.prepareToLeaveProcess(context);
             IIntentSender target =
@@ -724,7 +727,7 @@ public final class PendingIntent implements Parcelable {
             Intent intent, int flags, int serviceKind) {
         String packageName = context.getPackageName();
         String resolvedType = intent.resolveTypeIfNeeded(context.getContentResolver());
-        checkFlags(flags, packageName);
+        flags = checkFlags(flags, packageName);
         try {
             intent.prepareToLeaveProcess(context);
             IIntentSender target =
