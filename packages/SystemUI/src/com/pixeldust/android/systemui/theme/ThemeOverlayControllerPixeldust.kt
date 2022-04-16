bc56213f9106d9a5b5a9a2e9672b1a4152526556
Pranav Vashi <neobuddy89@gmail.com>
2022-04-16 10:38:36 +0530

    Fixes and improvements for Black Theme
    * Add to SettingsObserver for observing toggle behavior.
    * Apply only with dark theme - looks awful on light mode.
    * Add configuration listener to reapply theme when dark mode changes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I807e0d3c5ec2a443128eb86b506b8befaf3ccd52

--- packages/SystemUI/src/com/pixeldust/android/systemui/theme/ThemeOverlayControllerPixeldust.kt
+++ packages/SystemUI/src/com/pixeldust/android/systemui/theme/ThemeOverlayControllerPixeldust.kt
@@ -34,6 +34,7 @@ import com.android.systemui.dump.DumpManager
 import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.keyguard.WakefulnessLifecycle
 import com.android.systemui.settings.UserTracker
+import com.android.systemui.statusbar.policy.ConfigurationController
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
 import com.android.systemui.theme.ThemeOverlayApplier
 import com.android.systemui.theme.ThemeOverlayController
@@ -70,6 +71,7 @@ class ThemeOverlayControllerPixeldust @Inject constructor(
     dumpManager: DumpManager,
     featureFlags: FeatureFlags,
     wakefulnessLifecycle: WakefulnessLifecycle,
+    configurationController: ConfigurationController,
 ) : ThemeOverlayController(
     context,
     broadcastDispatcher,
@@ -85,6 +87,7 @@ class ThemeOverlayControllerPixeldust @Inject constructor(
     dumpManager,
     featureFlags,
     wakefulnessLifecycle,
+    configurationController,
 ), Tunable {
     private lateinit var cond: Zcam.ViewingConditions
     private lateinit var targets: MaterialYouTargets
