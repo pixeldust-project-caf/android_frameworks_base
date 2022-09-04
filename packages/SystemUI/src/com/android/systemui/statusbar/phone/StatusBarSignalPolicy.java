c31037f68c354e5ad79d10ccc24a490e57f90d7f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-04 18:19:00 +0530

    SystemUI: Switch to separate drawables for wifi standard
    This enables us to use wifi icon from icon pack,
    along with wifi standard.
    
    Inspired by https://github.com/Project-Kaleidoscope/android_frameworks_base/commit/1302dda146674906783030ff2846b3c17bcf50ad.
    
    Change-Id: Icc35df904b8f72cd2ac8ccc1d275e48d423909b0
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -211,6 +211,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
             newState.contentDescription = indicators.statusIcon.contentDescription;
             MobileIconState first = getFirstMobileState();
             newState.signalSpacerVisible = first != null && first.typeId != 0;
+            newState.wifiStandard = indicators.wifiStandard;
         }
         newState.slot = mSlotWifi;
         newState.airplaneSpacerVisible = mIsAirplaneMode;
@@ -560,6 +561,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
         public boolean noDefaultNetwork;
         public boolean noValidatedNetwork;
         public boolean noNetworksAvailable;
+        public int wifiStandard;
 
         @Override
         public boolean equals(Object o) {
@@ -576,7 +578,8 @@ public class StatusBarSignalPolicy implements SignalCallback,
                     && signalSpacerVisible == that.signalSpacerVisible
                     && noDefaultNetwork == that.noDefaultNetwork
                     && noValidatedNetwork == that.noValidatedNetwork
-                    && noNetworksAvailable == that.noNetworksAvailable;
+                    && noNetworksAvailable == that.noNetworksAvailable
+                    && wifiStandard == that.wifiStandard;
         }
 
         public void copyTo(WifiIconState other) {
@@ -587,6 +590,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
             other.noDefaultNetwork = noDefaultNetwork;
             other.noValidatedNetwork = noValidatedNetwork;
             other.noNetworksAvailable = noNetworksAvailable;
+            other.wifiStandard = wifiStandard;
         }
 
         public WifiIconState copy() {
