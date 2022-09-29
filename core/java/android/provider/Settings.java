2cdee03836d9a9a660ef428ab8c1d637931b054f
jhonboy121 <alfredmathew05@gmail.com>
2022-09-29 21:13:17 +0530

    base: merge hidden app feature with applock
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11067,12 +11067,6 @@ public final class Settings {
         public static final String TORCH_LONG_PRESS_POWER = "torch_long_press_power";
 
         /**
-         * List of ; separated apps to be hidden from launcher.
-         * @hide
-         */
-        public static final String LAUNCHER_HIDDEN_APPS = "launcher_hidden_apps";
-
-        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
