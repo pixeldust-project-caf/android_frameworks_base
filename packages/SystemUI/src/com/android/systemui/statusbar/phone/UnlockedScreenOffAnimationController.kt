omnirom/android_frameworks_base_old

SystemUI: disable keyguard screen off animation if we use split notification shade
the animation is messed up showing expanded qs while transition to aod

Change-Id: Ib4655af44a19609e5dc96275771ea5cb95fcfad2

packages/SystemUI/src/com/android/systemui/statusbar/phone/UnlockedScreenOffAnimationController.kt
@@ -22,6 +22,7 @@ import com.android.systemui.statusbar.notification.PropertyAnimator
 import com.android.systemui.statusbar.notification.stack.AnimationProperties
 import com.android.systemui.statusbar.notification.stack.StackStateAnimator
 import com.android.systemui.statusbar.policy.KeyguardStateController
+import com.android.systemui.util.Utils
 import com.android.systemui.util.settings.GlobalSettings
 import javax.inject.Inject
 
@@ -261,6 +262,10 @@ class UnlockedScreenOffAnimationController @Inject constructor(
         if (!keyguardStateController.isKeyguardScreenRotationAllowed &&
             context.display.rotation != Surface.ROTATION_0) {
             return false
+        }
+
+        if (Utils.shouldUseSplitNotificationShade(context.resources)) {
+            return false;
         }
 
         // Otherwise, good to go.

pixeldust-project-caf/android_frameworks_base

(cherry picked from commit e0f0aae4b5a7801a8e4f456f96a716333518ff5c)
Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SystemUI/src/com/android/systemui/statusbar/phone/UnlockedScreenOffAnimationController.kt
@@ -26,6 +26,7 @@ import com.android.systemui.statusbar.notification.PropertyAnimator
 import com.android.systemui.statusbar.notification.stack.AnimationProperties
 import com.android.systemui.statusbar.notification.stack.StackStateAnimator
 import com.android.systemui.statusbar.policy.KeyguardStateController
+import com.android.systemui.util.LargeScreenUtils
 import com.android.systemui.util.settings.GlobalSettings
 import javax.inject.Inject
 
@@ -327,6 +328,10 @@ class UnlockedScreenOffAnimationController @Inject constructor(
         if (!keyguardStateController.isKeyguardScreenRotationAllowed &&
             context.display.rotation != Surface.ROTATION_0) {
             return false
+        }
+
+        if (LargeScreenUtils.shouldUseSplitNotificationShade(context.resources)) {
+            return false;
         }
 
         // Otherwise, good to go.