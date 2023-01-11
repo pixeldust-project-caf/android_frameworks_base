71ce59de986dbaae4827e883128514d7a0de9fe7
spezi77 <spezi7713@gmx.net>
2023-01-12 00:02:51 +0100

    SystemUI: Dynamically adjust Lockscreen clock font spacing. Checks whether the currently set clock font requires a custom line spacing multiplier between hours and minutes of the keyguard clock.
    Partially based on: https://github.com/ricedroidOSS/android_frameworks_base/commit/3fb92d9279ec2d4f3f679f23fd3c7f7468a197be
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
+++ core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
@@ -267,6 +267,23 @@ public class PixeldustUtils {
         return false;
     }
 
+    // Method to detect whether a clock font overlay is enabled or not
+    public static boolean isClockFontEnabled(String fontName) {
+        mOverlayService = new OverlayManager();
+        try {
+            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
+                    UserHandle.myUserId());
+            for (int i = 0, size = infos.size(); i < size; i++) {
+                if (infos.get(i).packageName.contains(fontName)) {
+                    return infos.get(i).isEnabled();
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public static class OverlayManager {
         private final IOverlayManager mService;
 
