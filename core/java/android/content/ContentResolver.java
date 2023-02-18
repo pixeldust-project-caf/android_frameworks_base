57c44ce05c9eecd95488a359bba0fbc35453ea4d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-02-18 07:44:06 +0200

    add getContext() method to ContentResolver
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/content/ContentResolver.java
+++ core/java/android/content/ContentResolver.java
@@ -3959,6 +3959,11 @@ public abstract class ContentResolver implements ContentInterface {
     @UnsupportedAppUsage
     private final Context mContext;
 
+    /** @hide */
+    public Context getContext() {
+        return mContext;
+    }
+
     @Deprecated
     @UnsupportedAppUsage
     final String mPackageName;
