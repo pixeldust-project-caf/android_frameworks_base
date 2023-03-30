c4de4b513683b441ca00fa496b787d0bba415c27
LuK1337 <priv.luk@gmail.com>
2023-03-30 20:54:59 +0200

    SystemUI: Avoid unnecessary keyguard indication controller recreation
    This basically prevents us from running into situation where there are
    two controllers running in the background.
    
    Change-Id: I13781e3dd6cb3c2f75efad77fed214bae8d9be87
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -343,12 +343,14 @@ public class KeyguardIndicationController {
             R.id.keyguard_indication_text_bottom);
         mInitialTextColorState = mTopIndicationView != null
                 ? mTopIndicationView.getTextColors() : ColorStateList.valueOf(Color.WHITE);
-        mRotateTextViewController = new KeyguardIndicationRotateTextViewController(
+        if (mRotateTextViewController == null || !mRotateTextViewController.isAttachedToWindow()) {
+            mRotateTextViewController = new KeyguardIndicationRotateTextViewController(
                 mLockScreenIndicationView,
                 mExecutor,
                 mStatusBarStateController,
                 mKeyguardLogger
-        );
+            );
+        }
         mBatteryBar = indicationArea.findViewById(R.id.battery_bar_view);
         updateDeviceEntryIndication(false /* animate */);
         updateOrganizedOwnedDevice();
